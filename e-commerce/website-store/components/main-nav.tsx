4474592bdcc614d0c46484d317b6b5f22584fd82
cn05 <cucunurhayat05@gmail.com>
2024-10-22 23:57:52 +0700

    navbar

---
+++ e-commerce/website-store/components/main-nav.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { FC } from "react";
+
+interface MainNavProps {
+  data: any;
+}
+
+const MainNav: FC<MainNavProps> = ({ data }) => {
+  const pathname = usePathname();
+
+  const routes = data.map((route) => ({
+    href: `/category/${route.id}`,
+    label: route.name,
+    active: pathname === `/category/${route.id}`,
+  }));
+
+  return (
+    <nav className="mx-6 flex items-center space-x-4 lg:space-x-6">
+      {routes.map((route) => (
+        <Link key={route.href} href={route.href} className="">
+          {route.label}
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+export default MainNav;
